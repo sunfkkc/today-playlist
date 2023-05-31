64f92d2306653393a4b41ef6640969d91784387b
sunfkkc <rlcks77@nate.com>
2023-06-01 01:50:42 +0900

    feat: Button 컴포넌트 생성

---
+++ src/components/Button.tsx
@@ -0,0 +1,45 @@
+import { PropsWithChildren } from 'react';
+import classNames from 'classnames';
+import Text from '@/components/Text';
+
+type Props = PropsWithChildren<{
+  type?: 'primary' | 'danger' | 'light' | 'dark';
+  style?: 'fill' | 'outline' | 'weak' | 'flat';
+  display?: 'inline' | 'block' | 'full';
+
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+  disabled?: boolean;
+}>;
+function Button(props: Props) {
+  const {
+    type = 'primary',
+    style = 'fill',
+    display = 'inline',
+    disabled,
+    className,
+    children,
+    ...rest
+  } = props;
+
+  return (
+    <button
+      className={classNames(
+        'button',
+        'button--size-big',
+        {
+          [`button--type-${type}`]: type,
+          [`button--style-${style}`]: style,
+          [`button--display-${display}`]: display,
+        },
+        { disabled: disabled },
+        className
+      )}
+      {...rest}
+    >
+      <span className="button__content">{children}</span>
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import * as Icons from './icons';
+
+export { default as Text } from './Text';
+export { Icons };
+export { default as Button } from './Button';
