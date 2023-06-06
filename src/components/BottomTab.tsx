111b915012f637249de8b9cf1433ce5d36603b07
sunfkkc <rlcks77@nate.com>
2023-06-06 23:56:48 +0900

    update: 바텀 탭 컴포넌트를 화면 하단에 고정

--- src/components/BottomTab.tsx
+++ src/components/BottomTab.tsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 import Text from './Text';
 import { Icons } from '.';
 import { colors } from '@/constants/colors';
+import { GlobalPortal } from '@/GlobalPortal';
+import { css } from '@emotion/react';
+//TODO: 모바일에서 탭 선택시 backdrop이 남아있는 문제
 
 const menus = [
   { title: '홈', icon: 'Home24' },
@@ -18,44 +21,64 @@ function BottomTab() {
   const [selected, setSelected] = useState<Titles>('홈');
 
   return (
-    <div className={classNames('bottom-tab')}>
-      {menus.map((menu) => {
-        const Icon = Icons[menu.icon];
-        return (
-          <div
-            key={menu.title}
-            className={classNames('bottom-tab__item')}
-            onClick={() => {
-              setSelected(menu.title);
-            }}
-          >
-            {selected === menu.title && (
-              <div className={classNames('bottom-tab__item__backdrop')}></div>
-            )}
-            <div
-              className={classNames('bottom-tab__item__icon', {
-                'bottom-tab-selected': selected === menu.title,
-              })}
-            >
-              <Icon
-                width={24}
-                height={24}
-                stroke={selected === menu.title ? colors.blue600 : colors.white}
-              />
-            </div>
-
-            <div className={classNames('bottom-tab__item__title')}>
-              <Text
-                typography="cp"
-                color={selected === menu.title ? colors.blue600 : colors.white}
+    <GlobalPortal.Consumer>
+      <div
+        css={css`
+          position: fixed;
+          bottom: 0;
+          width: 100%;
+          max-width: 350px;
+          @media (max-width: 500px) {
+            max-width: none;
+          }
+        `}
+      >
+        <div className={classNames('bottom-tab')}>
+          {menus.map((menu) => {
+            const Icon = Icons[menu.icon];
+            return (
+              <div
+                key={menu.title}
+                className={classNames('bottom-tab__item')}
+                onClick={() => {
+                  setSelected(menu.title);
+                }}
               >
-                {menu.title}
-              </Text>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+                {selected === menu.title && (
+                  <div
+                    className={classNames('bottom-tab__item__backdrop')}
+                  ></div>
+                )}
+                <div
+                  className={classNames('bottom-tab__item__icon', {
+                    'bottom-tab-selected': selected === menu.title,
+                  })}
+                >
+                  <Icon
+                    width={24}
+                    height={24}
+                    stroke={
+                      selected === menu.title ? colors.blue600 : colors.white
+                    }
+                  />
+                </div>
+
+                <div className={classNames('bottom-tab__item__title')}>
+                  <Text
+                    typography="cp"
+                    color={
+                      selected === menu.title ? colors.blue600 : colors.white
+                    }
+                  >
+                    {menu.title}
+                  </Text>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </GlobalPortal.Consumer>
   );
 }
 
