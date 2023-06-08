944805bbafba2d721856214460fa7d126e167094
sunfkkc <rlcks77@nate.com>
2023-06-09 01:04:45 +0900

    chore: 리코일, 리액트쿼리, axios 설치 및 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,23 @@ import '@/sass/app.scss';
 import type { AppProps } from 'next/app';
 import { Layout } from '@/components';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { RecoilRoot } from 'recoil';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </GoogleOAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <GoogleOAuthProvider
+          clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
+        >
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </GoogleOAuthProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
