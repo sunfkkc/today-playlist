88964ea4aac95a4288d99af6e9325e63bca3aafd
sunfkkc <rlcks77@nate.com>
2023-06-09 01:01:28 +0900

    chore: GoogleOAuthProvider 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import '@/styles/globals.css';
 import '@/sass/app.scss';
 import type { AppProps } from 'next/app';
 import { Layout } from '@/components';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </GoogleOAuthProvider>
   );
 }
