bbae275e9bedfa64d17c57618abd77bc229345fe
sunfkkc <rlcks77@nate.com>
2023-06-15 14:36:51 +0900

    refactor: queryClient refetchOnWindowFocus 비활성화

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { RecoilRoot } from 'recoil';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
