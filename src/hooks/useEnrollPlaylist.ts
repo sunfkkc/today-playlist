1d37e59a8564da2b46009529d34b3189316a8508
sunfkkc <rlcks77@nate.com>
2023-07-24 00:55:26 +0900

    update: 플리 등록 성공 시 홈으로 돌아가기

--- src/hooks/useEnrollPlaylist.ts
+++ src/hooks/useEnrollPlaylist.ts
@@ -1,5 +1,9 @@
-import { EnrollPlaylistForm } from '@/atoms/enrollPlaylistForm';
+import {
+  EnrollPlaylistForm,
+  useEnrollPlaylistForm,
+} from '@/atoms/enrollPlaylistForm';
 import http from '@/http';
+import { useRouter } from 'next/router';
 import { useMutation } from 'react-query';
 
 const enrollPlaylist = async (form: EnrollPlaylistForm) => {
@@ -8,7 +12,7 @@ const enrollPlaylist = async (form: EnrollPlaylistForm) => {
   formData.append('file', form.image!);
   formData.append('title', form.title!);
   formData.append('hashtag', JSON.stringify(form.hashtag));
-  formData.append('videoId', JSON.stringify(form.videoId));
+  formData.append('videoId', JSON.stringify(form.videoId?.map((v) => v.id)));
 
   await http.post('/playlists/register', formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
@@ -16,7 +20,15 @@ const enrollPlaylist = async (form: EnrollPlaylistForm) => {
 };
 
 const useEnrollPlaylist = () => {
-  return useMutation(enrollPlaylist);
+  const router = useRouter();
+  const [_, setForm] = useEnrollPlaylistForm();
+
+  return useMutation(enrollPlaylist, {
+    onSuccess: () => {
+      router.push('/');
+      setForm({ hashtag: [], videoId: [] });
+    },
+  });
 };
 
 export default useEnrollPlaylist;
