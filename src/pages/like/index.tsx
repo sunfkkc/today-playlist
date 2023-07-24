f6494ea6b83a40a90fb4fb3ba1d33e9f8dc39d1c
sunfkkc <rlcks77@nate.com>
2023-07-25 01:33:07 +0900

    update: 상세페이지 라우팅, 검색 기능 추가

--- src/pages/like/index.tsx
+++ src/pages/like/index.tsx
@@ -1,20 +1,35 @@
-import { BottomTab, Header, TextFieldLine } from '@/components';
+import { BottomTab, Header, Icons, TextFieldLine } from '@/components';
 import PlaylistItem from '@/components/PlaylistItem';
+import { colors } from '@/constants/colors';
 import useLayoutHeight from '@/hooks/useLayoutHeight';
 import usePlaylists from '@/hooks/usePlaylists';
 import useScrollEndDetection from '@/hooks/useScrollEndDetection';
 import styled from '@emotion/styled';
-import { useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+import { FormEvent, useCallback, useEffect, useRef, useState } from 'react';
 
 const ITEM_HEIGHT = 164;
 
 function Page() {
+  const router = useRouter();
   const ref = useRef<HTMLDivElement>(null);
   const isBottom = useScrollEndDetection(ref);
   const height = useLayoutHeight();
+  const [_searchWord, _setSearchWord] = useState('');
+  const [searchWord, setSearchWord] = useState('');
   const { playlists, fetchNextPage } = usePlaylists({
-    size: 4,
+    size: height && Math.ceil(height / ITEM_HEIGHT) * 2,
+    isLiked: true,
+    searchWord,
   });
+
+  const search = useCallback(
+    (evt: FormEvent<HTMLFormElement>) => {
+      evt.preventDefault();
+      setSearchWord(_searchWord);
+    },
+    [setSearchWord, _searchWord]
+  );
 
   useEffect(() => {
     if (isBottom) {
@@ -24,11 +39,34 @@ function Page() {
 
   return (
     <div ref={ref} className="homepage-container">
-      <Header title="찜한 플리" />
-      <TextFieldLine />
-      {playlists?.map((props) => (
-        <PlaylistItem key={props.playlistId} {...props} />
-      ))}
+      <form onSubmit={search}>
+        <Header title="찜한 플리" />
+        <TextFieldLine
+          value={_searchWord}
+          onChange={(evt) => _setSearchWord(evt.target.value)}
+          placeholder="검색어를 입력해주세요."
+          fontColor={colors.white}
+          placeholderColor={colors.white}
+          inputAdornment={{
+            start: (
+              <Icons.Search24
+                width={24}
+                height={24}
+                stroke={colors.white}
+                style={{ marginRight: 4 }}
+              />
+            ),
+          }}
+        />
+        {playlists?.map((props) => (
+          <PlaylistItem
+            key={props.playlistId}
+            {...props}
+            style={{ marginBottom: 16, cursor: 'pointer' }}
+            onClick={() => router.push(`/detail/${props.playlistId}`)}
+          />
+        ))}
+      </form>
       <BottomTab />
     </div>
   );
