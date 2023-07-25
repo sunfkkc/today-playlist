f0a718cea55a866c82cf7c36f1fa042fb74c150b
sunfkkc <rlcks77@nate.com>
2023-07-25 17:25:31 +0900

    update: 스타일 적용

--- src/pages/enroll/index.tsx
+++ src/pages/enroll/index.tsx
@@ -1,7 +1,14 @@
 import { css } from '@emotion/react';
 import React, { ChangeEvent, useCallback, useRef, useState } from 'react';
 import Image from 'next/image';
-import { Button, Header, Icons, Text, TextFieldLine } from '@/components';
+import {
+  Button,
+  Divider,
+  Header,
+  Icons,
+  Text,
+  TextFieldLine,
+} from '@/components';
 import { colors } from '@/constants/colors';
 import { useEnrollPlaylistForm } from '@/atoms/enrollPlaylistForm';
 import useEnrollPlaylist from '@/hooks/useEnrollPlaylist';
@@ -90,25 +97,54 @@ function Page() {
         placeholderColor={colors.grey500}
         fontColor={colors.grey900}
         onChange={(evt) => setTitle(evt.target.value)}
+        containerStyle={{ marginTop: 16, marginBottom: 16 }}
+        style={{ fontSize: '14px', fontWeight: 400 }}
       />
       <PlaylistContainer>
-        <Text
-          typography="sh2"
-          color={colors.grey800}
-          fontWeight="bold"
-        >{`플레이리스트 추가`}</Text>
-        <Icons.Plus24
-          width={24}
-          height={24}
-          stroke={colors.grey500}
-          onClick={() => router.push('/add')}
-        />
+        <div
+          css={css`
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          `}
+        >
+          <Text
+            typography="sh2"
+            color={colors.grey800}
+            fontWeight="bold"
+          >{`플레이리스트 추가`}</Text>
+          <Icons.Plus24
+            width={24}
+            height={24}
+            stroke={colors.grey500}
+            onClick={() => router.push('/add')}
+            style={{ cursor: 'pointer' }}
+          />
+        </div>
         {form.videoId?.length !== 0 && (
-          <div>
+          <div
+            css={css`
+              margin-top: 10px;
+            `}
+          >
             {form.videoId?.map((v, i) => (
-              <div key={i}>
-                <Text>{v.title}</Text>
-              </div>
+              <React.Fragment key={i}>
+                <Item>
+                  <Text
+                    typography="cp"
+                    fontWeight="regular"
+                    color={colors.grey900}
+                  >
+                    {v.title}
+                  </Text>
+                  <Text
+                    typography="cp"
+                    fontWeight="regular"
+                    color={colors.grey700}
+                  >{`3:35`}</Text>
+                </Item>
+                <Divider opacity={0.16} backgroundColor={colors.grey800} />
+              </React.Fragment>
             ))}
           </div>
         )}
@@ -124,43 +160,54 @@ function Page() {
           onChange={(evt) => setTag(evt.target.value)}
           placeholderColor={colors.black}
           placeholder="# 태그를 입력해주세요. (최대 5개)"
+          placeholderStyle={{ fontSize: '12px', fontWeight: 400 }}
+          containerStyle={{ marginTop: 10 }}
+          style={{ fontWeight: 700, fontSize: '12px' }}
           inputAdornment={{
             end: (
-              <div
-                css={css`
-                  height: 32px;
-                  width: 56px;
-                `}
-              >
-                <div onClick={addTag}>
-                  <Text>{`추가`}</Text>
-                </div>
-              </div>
+              <AddButton onClick={() => addTag()}>
+                <Text
+                  typography="sh1"
+                  fontWeight="bold"
+                  color={colors.white}
+                >{`+추가`}</Text>
+              </AddButton>
             ),
           }}
         />
 
-        {form.hashtag?.length !== 0 &&
-          form.hashtag?.map((v, i) => (
-            <div
-              key={i}
-              css={css`
-                display: inline-block;
-                border-radius: 16px;
-                background: rgba(255, 255, 255, 0.15);
-              `}
-            >
-              <Text>{`#${v}`}</Text>
-              <Icons.X20Filled
-                width={20}
-                height={20}
-                fill={colors.grey400}
-                onClick={() => removeTag(i)}
-              />
-            </div>
-          ))}
+        {form.hashtag?.length !== 0 && (
+          <div
+            css={css`
+              display: flex;
+              flex-wrap: wrap;
+            `}
+          >
+            {form.hashtag?.map((v, i) => (
+              <TagItem key={i}>
+                <Text
+                  typography="cp"
+                  fontWeight="regular"
+                  color="#586589"
+                >{`#${v}`}</Text>
+                <Icons.X20Filled
+                  width={20}
+                  height={20}
+                  fill={colors.grey400}
+                  onClick={() => removeTag(i)}
+                  style={{ cursor: 'pointer' }}
+                />
+              </TagItem>
+            ))}
+          </div>
+        )}
       </TagContainer>
       <Button onClick={() => mutate(form)}>{`등록하기`}</Button>
+      <div
+        css={css`
+          margin-bottom: 32px;
+        `}
+      />
     </div>
   );
 }
@@ -190,28 +237,6 @@ const ImageContainer = styled.div`
 `;
 
 const PlaylistContainer = styled.div`
-  height: 58px;
-  flex-shrink: 0;
-  border-radius: 25px;
-  background: linear-gradient(
-      275.5deg,
-      rgba(255, 255, 255, 0.8) 0%,
-      rgba(255, 255, 255, 0) 54.4%
-    ),
-    linear-gradient(
-      264.5deg,
-      rgba(255, 255, 255, 0) 45.6%,
-      rgba(255, 255, 255, 0.8) 100%
-    );
-  box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04) inset;
-  backdrop-filter: blur(8px);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-`;
-
-const TagContainer = styled.div`
   flex-shrink: 0;
   border-radius: 25px;
   background: linear-gradient(
@@ -227,4 +252,50 @@ const TagContainer = styled.div`
   box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04) inset;
   backdrop-filter: blur(8px);
   padding: 20px;
+  margin-bottom: 16px;
+`;
+
+const TagContainer = styled.div`
+  border-radius: 25px;
+  background: linear-gradient(
+      275.5deg,
+      rgba(255, 255, 255, 0.8) 0%,
+      rgba(255, 255, 255, 0) 54.4%
+    ),
+    linear-gradient(
+      264.5deg,
+      rgba(255, 255, 255, 0) 45.6%,
+      rgba(255, 255, 255, 0.8) 100%
+    );
+  box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04) inset;
+  backdrop-filter: blur(8px);
+  padding: 20px;
+  margin-bottom: 16px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  align-items: center;
+  padding: 5px 0;
+`;
+
+const AddButton = styled.div`
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.06), rgba(0, 0, 0, 0.06)),
+    linear-gradient(114.48deg, rgba(252, 190, 204, 0.4) 0%, #c0deff 83.65%);
+  border-radius: 100px;
+  padding: 4px 8px 4px 8px;
+  cursor: pointer;
+`;
+
+const TagItem = styled.div`
+  border-radius: 16px;
+  background: rgba(255, 255, 255, 0.15);
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  margin-top: 8px;
+  margin-right: 8px;
+  padding: 8px 12px;
 `;
