0ade17e178e686a3c803fb3a80e7d773024194e2
sunfkkc <rlcks77@nate.com>
2023-07-26 15:49:57 +0900

    feat: 최근 본 플리 페이지

---
+++ src/pages/recent/index.tsx
@@ -0,0 +1,36 @@
+import { Header } from '@/components';
+import PlaylistItem from '@/components/PlaylistItem';
+import usePlaylists from '@/hooks/usePlaylists';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
+
+function Page() {
+  const router = useRouter();
+  const ref = useRef<HTMLDivElement>(null);
+  const { playlists } = usePlaylists({
+    ref,
+    itemHeight: 164,
+    recentViewed: true,
+  });
+  return (
+    <div className="homepage-container" ref={ref}>
+      <Header title="최근 본 플리" />
+      <div
+        css={css`
+          margin-bottom: 16px;
+        `}
+      />
+      {playlists?.map((props) => (
+        <PlaylistItem
+          key={props.playlistId}
+          {...props}
+          style={{ marginBottom: 16, cursor: 'pointer' }}
+          onClick={() => router.push(`/detail/${props.playlistId}`)}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Page;
