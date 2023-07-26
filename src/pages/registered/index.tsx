7bcf858cf31d43a0142314b6e58431f0b066d238
sunfkkc <rlcks77@nate.com>
2023-07-26 15:50:04 +0900

    feat: 등록한 플리 페이지

---
+++ src/pages/registered/index.tsx
@@ -0,0 +1,40 @@
+import { Header } from '@/components';
+import PlaylistItem from '@/components/PlaylistItem';
+import usePlaylists from '@/hooks/usePlaylists';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import { useCallback, useRef } from 'react';
+
+function Page() {
+  const router = useRouter();
+
+  const ref = useRef<HTMLDivElement>(null);
+  const { playlists } = usePlaylists({ ref, itemHeight: 116 });
+
+  const edit = useCallback(() => {
+    router.push('/enroll');
+  }, [router]);
+
+  return (
+    <div className="homepage-container" ref={ref}>
+      <Header title="등록한 플리" />
+      <div
+        css={css`
+          margin-bottom: 16px;
+        `}
+      />
+      {playlists?.map(({ hashtag, ...props }) => (
+        <PlaylistItem
+          key={props.playlistId}
+          {...props}
+          style={{ marginBottom: 16, cursor: 'pointer' }}
+          onClick={() => router.push(`/detail/${props.playlistId}`)}
+          editable
+          edit={edit}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Page;
