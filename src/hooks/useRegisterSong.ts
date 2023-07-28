143ba7d28e94dbd168cebbbac89626edd8f4f99a
sunfkkc <rlcks77@nate.com>
2023-07-28 15:24:52 +0900

    feat: useRegisterSong

---
+++ src/hooks/useRegisterSong.ts
@@ -0,0 +1,16 @@
+import http from '@/http';
+import { useMutation } from 'react-query';
+
+const registerSong = async (params: Params) => {
+  await http.post('/playlists/register/song', { params });
+};
+
+const useRegisterSong = () => {
+  return useMutation(registerSong);
+};
+export default useRegisterSong;
+
+interface Params {
+  videoId?: string;
+  title?: string;
+}
