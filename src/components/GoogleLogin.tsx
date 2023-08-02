867644dd24f39514573f2b89be6ee7c52d5b6dec
sunfkkc <rlcks77@nate.com>
2023-08-02 15:32:25 +0900

    fix: google login

--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -10,15 +10,18 @@ import useGoogle from '@/hooks/useGoogleLogin';
 function Login() {
   const { mutate } = useGoogle();
 
-  const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      mutate(tokenResponse);
-    },
-    flow: 'auth-code',
-  });
+  // const onClick = () => {
+  //   const client_id = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  //   const redirect_uri = 'https://api.todayplaylist.site/auth/google/redirect';
+
+  //   window.open(
+  //     `https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=openid+profile+email`,
+  //     '_blank'
+  //   );
+  // };
 
   return (
-    <Contaier onClick={() => login()}>
+    <Contaier onClick={() => mutate()}>
       <Google />
       <Text
         typography="sh2"
--- src/hooks/useGoogleLogin.ts
+++ src/hooks/useGoogleLogin.ts
@@ -1,8 +1,18 @@
 import http from '@/http';
+import axios from 'axios';
 import { useMutation } from 'react-query';
 
-const googleLogin = async (token?: any) => {
-  const { data } = await http.get(`/auth/google/redirect?code=${token.code}`);
+const googleLogin = async () => {
+  const client_id = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  const redirect_uri = 'https://api.todayplaylist.site/auth/google/redirect';
+  const response_type = 'code';
+  const scope = 'openid profile email';
+  const { data } = await axios.get(
+    'https://accounts.google.com/o/oauth2/v2/auth',
+    {
+      params: { client_id, redirect_uri, response_type, scope },
+    }
+  );
   return data;
 };
 
