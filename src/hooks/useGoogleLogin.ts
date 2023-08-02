68e1f512fcf875fb896c78fced8a8062babfd51b
sunfkkc <rlcks77@nate.com>
2023-08-02 13:58:45 +0900

    fix: google login

--- src/hooks/useGoogleLogin.ts
+++ src/hooks/useGoogleLogin.ts
@@ -2,7 +2,11 @@ import http from '@/http';
 import { useMutation } from 'react-query';
 
 const googleLogin = async () => {
-  const { data } = await http.get(`/auth/google`);
+  const { data } = await http.get(`/auth/google`, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
   return data;
 };
 
