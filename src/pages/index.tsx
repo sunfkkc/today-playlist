fe797f4f7cb143b5c55fc2d55d72d1c312e6b359
sunfkkc <rlcks77@nate.com>
2023-08-03 15:15:15 +0900

    update: 홈 화면 검색 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,32 +4,66 @@ import usePlaylists from '@/hooks/usePlaylists';
 import { css } from '@emotion/react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRef } from 'react';
+import { useRouter } from 'next/router';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+
   const ref = useRef<HTMLDivElement>(null);
+
+  const [_searchWord, _setSearchWord] = useState('');
+  const [searchWord, setSearchWord] = useState('');
+
   const { playlists } = usePlaylists({
     itemHeight: 228,
     ref,
+    searchWord,
   });
+
+  const search = useCallback(() => {
+    setSearchWord(_searchWord);
+    router.replace({
+      pathname: router.pathname,
+      query: { searchWord: _searchWord },
+    });
+  }, [setSearchWord, _searchWord, router]);
+
+  useEffect(() => {
+    const __searchWord = router.query.searchWord as string;
+
+    if (__searchWord) {
+      _setSearchWord(__searchWord);
+    }
+  }, [router]);
 
   return (
     <div className="homepage-container" ref={ref}>
-      <TextFieldLine
-        placeholder="검색어를 입력해주세요."
-        fontColor={colors.white}
-        placeholderColor={colors.white}
-        inputAdornment={{
-          start: (
-            <Icons.Search24
-              width={24}
-              height={24}
-              stroke={colors.white}
-              style={{ marginRight: 4 }}
-            />
-          ),
+      <form
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          search();
         }}
-      />
+      >
+        <TextFieldLine
+          value={_searchWord}
+          onChange={(evt) => _setSearchWord(evt.target.value)}
+          placeholder="검색어를 입력해주세요."
+          fontColor={colors.white}
+          placeholderColor={colors.white}
+          inputAdornment={{
+            start: (
+              <Icons.Search24
+                width={24}
+                height={24}
+                stroke={colors.white}
+                style={{ marginRight: 4, cursor: 'pointer' }}
+                onClick={search}
+              />
+            ),
+          }}
+        />
+      </form>
       <div
         css={css`
           display: flex;
