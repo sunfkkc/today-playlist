fd3e9359aaeb6ac97a9699437492d639a316baa3
sunfkkc <rlcks77@nate.com>
2023-08-03 18:15:32 +0900

    feat: AppProvider를 만들고, url에 따른 폼 초기화

---
+++ src/AppProvider.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router';
+import React, { ReactElement, useEffect } from 'react';
+import { useResetEnrollPlaylistForm } from './atoms/enrollPlaylistForm';
+
+let before = '';
+
+function AppProvider({ children }: { children: ReactElement }) {
+  const router = useRouter();
+  const reset = useResetEnrollPlaylistForm();
+  useEffect(() => {
+    const cur = router.pathname;
+
+    if (cur !== '/add' && before === '/enroll') {
+      reset();
+    }
+    return () => {
+      before = cur;
+    };
+  }, [router.pathname, reset]);
+  return children;
+}
+
+export default AppProvider;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Layout } from '@/components';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { RecoilRoot } from 'recoil';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import AppProvider from '@/AppProvider';
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
@@ -17,9 +18,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <GoogleOAuthProvider
           clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
         >
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          <AppProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AppProvider>
         </GoogleOAuthProvider>
       </RecoilRoot>
     </QueryClientProvider>
