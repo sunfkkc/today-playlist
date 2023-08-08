0e24f125c28e0a6ac822469702142aa47438788f
sunfkkc <rlcks77@nate.com>
2023-08-08 13:24:16 +0900

    update: filterColors 추가

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -43,3 +43,63 @@ export const colors = {
   red100: '#fcbecc',
   red50: '#fee5eb',
 };
+
+export const filterColors = {
+  white:
+    'invert(100%) sepia(100%) saturate(9%) hue-rotate(191deg) brightness(103%) contrast(103%)',
+  black: '#000000',
+  primary900: '#192347',
+  primary800: '#283358',
+  primary700: '#2f3c65',
+  primary600: '#364570',
+  primary500: '#3d4c78',
+  primary400: '#586589',
+  primary300: '#74809c',
+  primary200: '#9aa3b9',
+  primary100: '#c1c7d5',
+  primary50: '#e6e9ee',
+  grey900: '#1d1d1d',
+  grey800: '#3d3d3d',
+  grey700: '#5c5c5c',
+  grey600:
+    'invert(44%) sepia(0%) saturate(3922%) hue-rotate(342deg) brightness(102%) contrast(97%)',
+  grey500:
+    'invert(73%) sepia(8%) saturate(31%) hue-rotate(323deg) brightness(83%) contrast(82%)',
+  grey400:
+    'invert(65%) sepia(66%) saturate(8%) hue-rotate(318deg) brightness(98%) contrast(97%)',
+  grey300: '#dbdbdb',
+  grey200: '#ebebeb',
+  grey100: '#f3f3f3',
+  grey50: '#f9f9f9',
+
+  blue800:
+    '#invert(35%) sepia(29%) saturate(3592%) hue-rotate(213deg) brightness(90%) contrast(87%)',
+  blue700:
+    'invert(38%) sepia(38%) saturate(4689%) hue-rotate(214deg) brightness(99%) contrast(85%)',
+  blue600:
+    'invert(73%) sepia(62%) saturate(7028%) hue-rotate(204deg) brightness(101%) contrast(104%)',
+  blue500:
+    'invert(54%) sepia(67%) saturate(2509%) hue-rotate(194deg) brightness(99%) contrast(105%)',
+  blue400:
+    'invert(52%) sepia(83%) saturate(935%) hue-rotate(189deg) brightness(101%) contrast(101%)',
+  blue300:
+    'invert(60%) sepia(42%) saturate(1984%) hue-rotate(190deg) brightness(110%) contrast(101%)',
+  blue200:
+    'invert(69%) sepia(45%) saturate(401%) hue-rotate(179deg) brightness(102%) contrast(101%)',
+  blue100:
+    'invert(100%) sepia(29%) saturate(6565%) hue-rotate(174deg) brightness(97%) contrast(108%)',
+  blue50: '#e5f2ff',
+  red900: '#901f4b',
+  red800: '#b42851',
+  red700: '#c92e54',
+  red600: '#de3458',
+  red500: '#ef3959',
+  red400:
+    'invert(56%) sepia(52%) saturate(6329%) hue-rotate(320deg) brightness(99%) contrast(92%)',
+  red300: '#f76b8a',
+  red200: '#fa94ab',
+  red100: '#fcbecc',
+  red50: '#fee5eb',
+  blue900:
+    'invert(25%) sepia(72%) saturate(1951%) hue-rotate(229deg) brightness(80%) contrast(94%)',
+};
