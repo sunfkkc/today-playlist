b4288dffcb79067de3b2b8d9baf59c11ebf022ab
sunfkkc <rlcks77@nate.com>
2023-08-13 11:49:41 +0900

    fix: 로그아웃 후 refetch

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,6 +1,6 @@
 import http from '@/http';
 import queryKeys from '@/constants/queryKeys';
-import { useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 
 const getUser = async () => {
   const { data } = await http.get('/auth');
@@ -8,16 +8,13 @@ const getUser = async () => {
 };
 
 const useUser = () => {
-  const queryClient = useQueryClient();
   const methods = useQuery<{ nickname: string; profileImgUrl: string }>(
     queryKeys.user,
     getUser
   );
   const logout = async () => {
     await http.delete('/auth/logout');
-    queryClient.invalidateQueries({
-      queryKey: queryKeys.user,
-    });
+    methods.refetch();
   };
   return { ...methods, logout };
 };
