7f83cd5aeceb560f3abc18c275a8c4c3c415788c
sunfkkc <rlcks77@nate.com>
2023-08-15 16:50:46 +0900

    refactor: 컨텍스트와 매개변수를 받을 수 있게 수정

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,16 +1,16 @@
-const throttle = (handler: () => void, timeout = 3000) => {
+const throttle = (handler: (...args: any[]) => void, timeout = 3000) => {
   let invokedTime: number;
   let timer: number;
 
-  return () => {
+  return function (this: any, ...args: any[]) {
     if (!invokedTime) {
-      handler();
+      handler.apply(this, args);
       invokedTime = Date.now();
     } else {
       clearTimeout(timer);
       timer = window.setTimeout(() => {
         if (Date.now() - invokedTime >= timeout) {
-          handler();
+          handler.apply(this, args);
           invokedTime = Date.now();
         }
       }, Math.max(timeout - (Date.now() - invokedTime), 0));
