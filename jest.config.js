054c7010e5d3d9b9eb12dc34ef0183c4c61a1481
sunfkkc <rlcks77@nate.com>
2023-08-21 10:03:35 +0900

    chore: 테스트를 위한 설정

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-  collectCoverage: true,
-  // on node 14.x coverage provider v8 offers good speed and more or less good report
-  coverageProvider: 'v8',
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-    '!<rootDir>/out/**',
-    '!<rootDir>/.next/**',
-    '!<rootDir>/*.config.js',
-    '!<rootDir>/coverage/**',
-  ],
   moduleNameMapper: {
     // Handle CSS imports (with CSS modules)
     // https://jestjs.io/docs/webpack#mocking-css-modules
@@ -41,4 +29,5 @@ module.exports = {
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$',
   ],
+  setupFilesAfterEnv: ['./jest.setup.js'],
 };
