cbd503163de9953eff2a1207749f2fa095cbc21d
sunfkkc <rlcks77@nate.com>
2023-08-29 16:10:40 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,7 @@ export default function Home() {
 
   return (
     <div className="homepage-container">
-      <div>무중단 배포 테스트6</div>
+      <div>마지막일듯</div>
       <form
         onSubmit={(evt) => {
           evt.preventDefault();
