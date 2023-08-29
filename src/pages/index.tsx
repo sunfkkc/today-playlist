ba58260063fb1d36bf98e2afd67efe1194d5adae
sunfkkc <rlcks77@nate.com>
2023-08-29 18:02:50 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,7 @@ export default function Home() {
 
   return (
     <div className="homepage-container">
-      <div>변경사항5</div>
+      <div>변경사항6</div>
       <form
         onSubmit={(evt) => {
           evt.preventDefault();
