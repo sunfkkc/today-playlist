481d672bb459c501158593b50cf03ea344e46c8f
sunfkkc <rlcks77@nate.com>
2023-09-03 17:26:41 +0900

    chore: 절대경로를 제대로 적용

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,10 @@
+#!/bin/bash
+
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 
-source ${ABSDIR}/stop.sh
-source ${ABSDIR}/start.sh
-source ${ABSDIR}/switch.sh
+PORT=$(${ABSDIR}/port.sh)
 
-PORT=$(./port.sh)
-
-./stop.sh $PORT
-
-./start.sh $PORT
-
-./switch.sh $PORT
+${ABSDIR}/stop.sh $PORT
+${ABSDIR}/start.sh $PORT
+${ABSDIR}/switch.sh $PORT
