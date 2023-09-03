61faa9d2e5dac5c886fd09dca94a1242e877cbe1
sunfkkc <rlcks77@nate.com>
2023-09-03 17:12:24 +0900

    chore: 절대 경로 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,9 @@
-source ./stop.sh
-source ./start.sh
-source ./switch.sh
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+
+source ${ABSDIR}/stop.sh
+source ${ABSDIR}/start.sh
+source ${ABSDIR}/switch.sh
 
 PORT=$(./port.sh)
 
