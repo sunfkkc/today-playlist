a02c8cecd2c9f7cef67035ab1d6cde0bb65561c6
sunfkkc <rlcks77@nate.com>
2023-09-04 12:27:54 +0900

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,9 @@
 TARGET_PORT=$1  # 첫 번째 매개변수로 전달된 포트 번호
 cd /home/ec2-user/app2/zip
 
-yarn
+~/.nvm/versions/node/v16.20.2/bin/yarn
 
-nohup yarn start -p $TARGET_PORT &
+nohup ~/.nvm/versions/node/v16.20.2/bin/yarn start -p $TARGET_PORT &
 
 sleep 5
 
