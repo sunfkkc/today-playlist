e5a23432b4b515760924d583b2a5561698a32a0e
sunfkkc <rlcks77@nate.com>
2023-09-07 14:25:43 +0900

    test: 태그 추가 테스트

--- __tests__/enroll.spec.tsx
+++ __tests__/enroll.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ImageUploader from '@/domain/enroll-playlist/components/ImageUploader';
 import TagUploader from '@/domain/enroll-playlist/components/TagUploader';
@@ -26,33 +26,27 @@ describe('플리 등록 테스트', () => {
   });
 
   it('태그가 입력되지 않으면 추가 되지 않는다.', async () => {
-    const mockedAddTag = jest.fn();
-    render(<TagUploader addTagHandler={mockedAddTag} />);
+    const mockedSetTags = jest.fn();
+    render(<TagUploader setTags={mockedSetTags} />);
 
     userEvent.click(await screen.findByTestId('add-button'));
 
-    expect(mockedAddTag).not.toHaveBeenCalled();
+    expect(mockedSetTags).not.toHaveBeenCalled();
   });
 
-  it('태그가 정상적으로 추가되고 삭제된다.', async () => {
-    const tags = ['새벽감성', '출퇴근길', '집중타임'];
+  it('태그가 정상적으로 추가된다', async () => {
+    const tag = '새벽감성';
 
-    render(<TagUploader />);
+    const mockedSetTags = jest.fn();
+
+    render(<TagUploader setTags={mockedSetTags} />);
 
     const input = await screen.findByTestId('input');
 
-    for (const tag of tags) {
-      fireEvent.change(input, { target: { value: tag } });
+    fireEvent.change(input, { target: { value: tag } });
 
-      userEvent.click(await screen.findByTestId('add-button'));
-      expect(await screen.findByText(`#${tag}`)).toBeInTheDocument();
-    }
+    userEvent.click(await screen.findByTestId('add-button'));
 
-    //TODO: 태그 삭제 테스트
-    // screen.debug();
-    // userEvent.click(await screen.findByTestId(`remove-button-1`));
-    // expect(screen.queryByText(`#${tags[1]}`)).not.toBeInTheDocument();
+    await waitFor(() => expect(mockedSetTags).toHaveBeenCalledTimes(1));
   });
-
-  //it('로그인 되지 않으면 마이페이지로 이동된다.', () => {});
 });
